488f6f5bb472c988282227cef00ecd686a28f5fe
Eddie Bonk <eddie@heysparkbox.com>
2013-02-27 16:26:00 -0500

    Refactored functions and added class structure

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,30 +1,23 @@
-def numbers 
-	(1..100).each do |x|
-		text_for_fizz_and_buzz(x)
+class Fizzbuzz
+	attr_reader :print_numbers, :test_for_fizz_and_buzz
+	
+	def print_numbers
+		(1..100).each do |x|
+			puts test_for_fizz_and_buzz(x)
+		end
+	end
+
+	def test_for_fizz_and_buzz(x)
+		if x % 3 == 0 && x % 5 == 0
+			return 'fizzbuzz'
+		elsif x % 3 == 0 && x % 5 != 0
+			return 'fizz'
+		elsif x % 5 == 0 && x % 3 != 0
+			return 'buzz'
+		else
+			return x
+		end
 	end
 end
 
-def text_for_fizz_and_buzz(x)
-	if x % 3 == 0 && x % 5 == 0
-		puts number_is_fizzbuzz
-	elsif x % 3 == 0 && x % 5 != 0
-		puts number_is_fizz
-	elsif x % 5 == 0 && x % 3 != 0
-		puts number_is_buzz
-	else
-		puts x
-	end
-end
-
-def number_is_fizz
-	return 'fizz'
-end
-
-def number_is_buzz
-	return 'buzz'
-end
-
-def number_is_fizzbuzz
-	return 'fizzbuzz'
-end
 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,15 +3,23 @@ require 'fizzbuzz'
 
 describe 'Fizzbuzz' do 
 	it 'should print numbers 1 to 100 numbers' do 
-		numbers.should == (1..100)
+		fizzbuzz = Fizzbuzz.new
+		fizzbuzz.print_numbers.should == (1..100)
 	end
-	it 'should print fizz when a number is divisible by 3' do 
-		number_is_fizz.should == 'fizz'
+	it 'should print fizz when the number is divisible by 3' do 
+		fizzbuzz = Fizzbuzz.new
+		fizzbuzz.test_for_fizz_and_buzz(3).should == 'fizz'
 	end
-	it 'should print buzz when a number is divisible by 5' do 
-		number_is_buzz.should == 'buzz'
+	it 'should print fizz when the number is divisible by 5' do 
+		fizzbuzz = Fizzbuzz.new
+		fizzbuzz.test_for_fizz_and_buzz(5).should == 'buzz'
 	end
-	it 'should print fizzbuzz when a number is divisible by 3 and 5' do 
-		number_is_fizzbuzz.should == 'fizzbuzz'
+	it 'should print fizz when the number is divisible by 3 and 5'do 
+		fizzbuzz = Fizzbuzz.new
+		fizzbuzz.test_for_fizz_and_buzz(15).should == 'fizzbuzz'
+	end
+	it 'should print the number when it is not divisible by 3, 5, or both' do 
+		fizzbuzz = Fizzbuzz.new
+		fizzbuzz.test_for_fizz_and_buzz(4).should == 4
 	end
 end
