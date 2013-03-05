6857fb3b8b69a7e7bb3663338d20b4b75c2f6f4b
Eddie Bonk <eddie@heysparkbox.com>
2013-03-05 09:12:27 -0500

    Fixed wording and organization in tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,20 +6,20 @@ describe 'Fizzbuzz' do
 		fizzbuzz = Fizzbuzz.new
 		fizzbuzz.print_numbers.should == (1..100)
 	end
+	it 'should print the number when it is not divisible by 3, 5, or both' do 
+		fizzbuzz = Fizzbuzz.new
+		fizzbuzz.test_for_fizz_and_buzz(4).should == 4
+	end
 	it 'should print fizz when the number is divisible by 3' do 
 		fizzbuzz = Fizzbuzz.new
 		fizzbuzz.test_for_fizz_and_buzz(3).should == 'fizz'
 	end
-	it 'should print fizz when the number is divisible by 5' do 
+	it 'should print buzz when the number is divisible by 5' do 
 		fizzbuzz = Fizzbuzz.new
 		fizzbuzz.test_for_fizz_and_buzz(5).should == 'buzz'
 	end
-	it 'should print fizz when the number is divisible by 3 and 5'do 
+	it 'should print fizzbuzz when the number is divisible by 3 and 5'do 
 		fizzbuzz = Fizzbuzz.new
 		fizzbuzz.test_for_fizz_and_buzz(15).should == 'fizzbuzz'
-	end
-	it 'should print the number when it is not divisible by 3, 5, or both' do 
-		fizzbuzz = Fizzbuzz.new
-		fizzbuzz.test_for_fizz_and_buzz(4).should == 4
 	end
 end
